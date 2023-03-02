7e558b73184e300d55e96d19b3938926724e0a94
av124773 <av124773@gmail.com>
2023-03-02 11:04:07 +0800

    feat: add shorten url function

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
  */
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const UrlShortener = require('./models/url-shortener')
+const urlRandomCodeCreate = require('./models/url-random')
 
 if (process.env.NODE_ENV = 'production') {
   require('dotenv').config()
@@ -10,6 +15,7 @@ if (process.env.NODE_ENV = 'production') {
 
 const app = express()
 const PORT = 3001
+const HOST = 'http://localhost:3001/'
 
 /** 
  * Mongoose connect setting
@@ -23,11 +29,42 @@ db.once('open', () => {
   console.log('mongoose connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 /** 
  * Route setting
  */
 app.get('/', (req, res) => {
-  res.send('server test.')
+  res.render('index') 
+})
+
+app.get('/:id', (req, res) => {
+  const id = req.params.id
+  return UrlShortener.findById(id)
+    .lean()
+    .then(urlShortener => res.render('show', { urlShortener }))
+    .catch(error => console.log(error))
+})
+
+app.post('/shorten', (req, res) => {
+  const userUrl = req.body.url
+  return UrlShortener.find({ url: userUrl })
+    .then(findUrl => {
+      if (findUrl.length === 0) {
+        console.log('no find')
+        const shortUrl = HOST + urlRandomCodeCreate()
+        return UrlShortener.create({ url: userUrl, shortUrl: shortUrl })
+          .then(urlShortener => res.redirect(`/${urlShortener._id}`))
+          .catch(error => console.log(error))
+      } else {
+        console.log('find url')
+        res.redirect(`/${findUrl[0]._id}`)
+      }
+    })
+    .catch(error => console.log(error)) 
 })
 
 /** 
